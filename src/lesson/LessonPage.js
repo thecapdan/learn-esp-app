2b5f828daac93a227f978cfcbcc9d96ed647ddbc
thecapdan <zdano80@gmail.com>
2021-05-18 20:21:26 +0100

    styling table on lesson page

--- src/lesson/LessonPage.js
+++ src/lesson/LessonPage.js
@@ -2,6 +2,7 @@ import React from "react";
 import { useLesson } from "./useLesson";
 import { BackButton, NavButton } from "../ui";
 import ReactMarkdown from "react-markdown";
+import gfm from "remark-gfm";
 
 export const LessonPage = () => {
   const { lesson, isLoading, hasExercise, hasVocab } = useLesson();
@@ -13,7 +14,7 @@ export const LessonPage = () => {
         <p>Loading...</p>
       ) : (
         <div className="centred-container">
-          <ReactMarkdown>{lesson.intro}</ReactMarkdown>
+          <ReactMarkdown remarkPlugins={[gfm]}>{lesson.intro}</ReactMarkdown>
           <div className="lesson-page-button-wrapper">
             <div className="btn-group special" role="group">
               <NavButton link="/" name="Home" />
